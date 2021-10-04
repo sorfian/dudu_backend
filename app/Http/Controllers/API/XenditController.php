945645ace0d1a63b6c0ea33bffe23e264f92b7e0
Sorfian <sorfian.tkj@gmail.com>
2021-10-04 10:22:15 +0000

    add xendit callback

--- app/Http/Controllers/API/XenditController.php
+++ app/Http/Controllers/API/XenditController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\UserTransaction;
 use Illuminate\Http\Request;
 
 class XenditController extends Controller
@@ -10,97 +11,31 @@ class XenditController extends Controller
     public function callback(Request $request)
     {
         $externalId = $request->external_id;
+        $xenditId = $request->id;
+        $paymentMethod = $request->payment_method;
+        $status = $request->status;
+        $amount = $request->amount;
+        $paymentChannel = $request->payment_channel;
 
-        return $externalId;
+        $transaction = UserTransaction::where('external_id', $externalId)->where('invoice_number', $xenditId)->first();
+        if ($status == 'PAID') {
+            $transaction->status = 'SUCCESS';
+        } else if ($status == 'EXPIRED') {
+            $transaction->status == 'CANCELLED';
+        } else {
+            $transaction->status == 'ON PROCESS';
+        }
 
-        // Set konfigurasi midtrans
-        // Config::$serverKey = config('services.midtrans.serverKey');
-        // Config::$isProduction = config('services.midtrans.isProduction');
-        // Config::$isSanitized = config('services.midtrans.isSanitized');
-        // Config::$is3ds = config('services.midtrans.is3ds');
-
-        // // Buat instance midtrans notification
-        // $notification = new Notification();
-
-        // // Assign ke variable untuk memudahkan coding
-        // $status = $notification->transaction_status;
-        // $type = $notification->payment_type;
-        // $fraud = $notification->fraud_status;
-        // $order_id = $notification->order_id;
-
-        // // Cari transaksi berdasarkan ID
-        // $transaction = Transaction::findOrFail($order_id);
-
-        // // Handle notification status midtrans
-        // if ($status == 'capture') {
-        //     if ($type == 'credit_card'){
-        //         if($fraud == 'challenge'){
-        //             $transaction->status = 'PENDING';
-        //         }
-        //         else {
-        //             $transaction->status = 'SUCCESS';
-        //         }
-        //     }
-        // }
-        // else if ($status == 'settlement'){
-        //     $transaction->status = 'SUCCESS';
-        // }
-        // else if($status == 'pending'){
-        //     $transaction->status = 'PENDING';
-        // }
-        // else if ($status == 'deny') {
-        //     $transaction->status = 'CANCELLED';
-        // }
-        // else if ($status == 'expire') {
-        //     $transaction->status = 'CANCELLED';
-        // }
-        // else if ($status == 'cancel') {
-        //     $transaction->status = 'CANCELLED';
-        // }
-
-        // // Simpan transaksi
-        // $transaction->save();
-
-        // // Kirimkan email
-        // if ($transaction)
-        // {
-        //     if($status == 'capture' && $fraud == 'accept' )
-        //     {
-        //         //
-        //     }
-        //     else if ($status == 'settlement')
-        //     {
-        //         //
-        //     }
-        //     else if ($status == 'success')
-        //     {
-        //         //
-        //     }
-        //     else if($status == 'capture' && $fraud == 'challenge' )
-        //     {
-        //         return response()->json([
-        //             'meta' => [
-        //                 'code' => 200,
-        //                 'message' => 'Midtrans Payment Challenge'
-        //             ]
-        //         ]);
-        //     }
-        //     else
-        //     {
-        //         return response()->json([
-        //             'meta' => [
-        //                 'code' => 200,
-        //                 'message' => 'Midtrans Payment not Settlement'
-        //             ]
-        //         ]);
-        //     }
-
-        //     return response()->json([
-        //         'meta' => [
-        //             'code' => 200,
-        //             'message' => 'Midtrans Notification Success'
-        //         ]
-        //     ]);
-        // }
+        $transaction->payment_method = $paymentMethod;
+        $transaction->payment_channel = $paymentChannel;
+        $transaction->total = $amount;
+        $transaction->save();
+        
+        return response()->json([
+            'meta' => [
+                'code' => 200,
+                'message' => 'Xendit payment status successfully saved!'
+            ]
+        ]);
     }
 }
