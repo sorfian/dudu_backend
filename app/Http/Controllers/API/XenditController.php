01ffe37a6b73de2018a7aeee9b46b94902d2b993
Sorfian <sorfian.tkj@gmail.com>
2021-10-04 10:25:01 +0000

    add xendit callback

--- app/Http/Controllers/API/XenditController.php
+++ app/Http/Controllers/API/XenditController.php
@@ -17,25 +17,35 @@ class XenditController extends Controller
         $amount = $request->amount;
         $paymentChannel = $request->payment_channel;
 
-        $transaction = UserTransaction::where('external_id', $externalId)->where('invoice_number', $xenditId)->first();
-        if ($status == 'PAID') {
-            $transaction->status = 'SUCCESS';
-        } else if ($status == 'EXPIRED') {
-            $transaction->status == 'CANCELLED';
-        } else {
-            $transaction->status == 'ON PROCESS';
-        }
+        try {
+            $transaction = UserTransaction::where('external_id', $externalId)->where('invoice_number', $xenditId)->first();
+            if ($status == 'PAID') {
+                $transaction->status = 'SUCCESS';
+            } else if ($status == 'EXPIRED') {
+                $transaction->status == 'CANCELLED';
+            } else {
+                $transaction->status == 'ON PROCESS';
+            }
 
-        $transaction->payment_method = $paymentMethod;
-        $transaction->payment_channel = $paymentChannel;
-        $transaction->total = $amount;
-        $transaction->save();
+            $transaction->payment_method = $paymentMethod;
+            $transaction->payment_channel = $paymentChannel;
+            $transaction->total = $amount;
+            $transaction->save();
+            
+            return response()->json([
+                'meta' => [
+                    'code' => 200,
+                    'message' => 'Xendit payment status successfully saved!'
+                ]
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'meta' => [
+                    'code' => 500,
+                    'message' => $th,
+                ]
+            ]);
+        }
         
-        return response()->json([
-            'meta' => [
-                'code' => 200,
-                'message' => 'Xendit payment status successfully saved!'
-            ]
-        ]);
     }
 }
